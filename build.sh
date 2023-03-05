e9e5aa53ac730c1807089c13473c89e79f4002c0
Tomas <tomasdaureli@gmail.com>
2023-03-05 12:26:59 -0300

    media

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
-
 if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input
 fi
+
+python manage.py collectstatic --no-input
+python manage.py migrate
